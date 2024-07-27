9674b9bd228d5ec17c7fc070c695b7f7e1bdd9ee
prashant <prashant292singh@gmail.com>
2024-07-27 22:26:24 +0530

    localhost3

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ async def predict(request: Request, Sepal_Length: float = Form(...), Sepal_Width
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app,port=8000)
